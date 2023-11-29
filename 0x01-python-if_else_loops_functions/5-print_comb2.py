008e78179ddd5c76c52d800745129e8907867b83
ShroukOuda <s36568961@gmail.com>
2023-11-29 04:14:18 +0200

    commit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 10:
-        print(0, end='')
-    print(i, end=', ' if i != 99 else '\n')
+        print("{}".format(0), end='')
+    print("{}".format(i), end=', ' if i != 99 else '\n')
