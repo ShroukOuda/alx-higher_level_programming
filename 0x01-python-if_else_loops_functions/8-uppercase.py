7f62ec57b0eb606a1ad377846180d1c49bd0f8cd
ShroukOuda <s36568961@gmail.com>
2023-11-29 05:24:29 +0200

    commit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(s):
+    for i in range(len(s)):
+        if ord(s[i]) > 96 and ord(s[i]) < 123:
+            print("{}".format(chr(ord(s[i]) - 32)),
+                  end='' if i != len(s) - 1 else '\n')
+        else:
+            print("{}".format(s[i]),
+                  end='' if i != len(s) - 1 else '\n')
