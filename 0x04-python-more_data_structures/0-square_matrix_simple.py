80125b16daa510bca9fcd1728d698eb30484bd80
ShroukOuda <s36568961@gmail.com>
2023-12-06 13:12:56 +0200

    commit

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+new_matrix = []
+def square_matrix_simple(matrix=[]):
+    for i in matrix:
+        row_matrix = []
+        for j in i:
+            result = j * j
+            row_matrix.append(result)
+        new_matrix.append(row_matrix)
+    return new_matrix
