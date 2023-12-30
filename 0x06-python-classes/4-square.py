bfe74b9ca09f9dd61836d8f39f0b56610b200b2f
shrouk_ouda <shroukouda2@gmail.com>
2023-12-30 02:22:33 -0500

    commit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,10 +6,6 @@ class Square:
     ''' square class '''
     def __init__(self, size=0):
         self.size = size
-        if not isinstance(self.size, int):
-            raise TypeError("size must be an integer")
-        if self.size < 0:
-            raise ValueError("size must be >= 0")
 
     def area(self):
         if not isinstance(self.size, int):
