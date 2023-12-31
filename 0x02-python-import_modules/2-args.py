adc07581332b9434d38a1fc4a2e40bc874874593
shrouk_ouda <shroukouda2@gmail.com>
2023-12-31 02:25:07 -0500

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-
+from sys import argv
+if __name__ == "__main__":
+    args_no = len(argv) - 1
+    arg_str = "argument" if args_no == 1 else "arguments"
+    print("{} {}{}".format(args_no, arg_str, '.' if args_no == 0 else ':'))
+    for i in range(1, args_no + 1):
+        print("{}: {}".format(i, argv[i]))
