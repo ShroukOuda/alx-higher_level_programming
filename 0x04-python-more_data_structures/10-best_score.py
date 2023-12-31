93d340bbcd7cb482087d37ba5c048fdf8be59d94
shrouk_ouda <shroukouda2@gmail.com>
2023-12-31 00:38:37 -0500

    commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    Max = 0
-    Key = ''
-    for key, value in a_dictionary.items():
-        if value > Max:
-            Max = value
-            Key = key
-    return Key
+    if a_dictionary:
+        Max = 0
+        Key = ''
+        for key, value in a_dictionary.items():
+            if value > Max:
+                Max = value
+                Key = key
+        return Key
+    return None
