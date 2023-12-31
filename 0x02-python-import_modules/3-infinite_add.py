a090a45b4d3831b8ba6999fc059c8bb6216e42f5
shrouk_ouda <shroukouda2@gmail.com>
2023-12-31 02:37:18 -0500

    commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
-res = 0
-for i in range(1, len(argv)):
-    res += int(argv[i])
-print(res)
+if __name__ == "__main__":
+    res = 0
+    for i in range(1, len(argv)):
+        res += int(argv[i])
+    print("{}".format(res))
