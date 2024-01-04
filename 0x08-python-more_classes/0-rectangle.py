767390925dfbe66bf75bb85ad446ac600a55e362
shrouk_ouda <shroukouda2@gmail.com>
2024-01-04 02:49:43 -0500

    commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+''' rectangle claSS '''
+
+
+class Rectangle:
+    '''rectangle class '''
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''rectangle class'''
+
+
+class Rectangle:
+    '''rectangle class'''
+    def __init__(self, width=0, height=0):
+        self._Rectangle__height = height
+        self._Rectangle__width = width
+
+    @property
+    def width(self):
+        return self._Rectangle__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._Rectangle__width = value
+
+    @property
+    def height(self):
+        return self._Rectangle__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._Rectangle__height = value
