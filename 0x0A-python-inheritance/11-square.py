b81b3bb67f239a1bed7a102c6d385642c5dc4054
shrouk_ouda <shroukouda2@gmail.com>
2024-01-14 12:08:40 -0500

    commit

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -34,3 +34,9 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return '[Square] {}/{}'.format(self.__size, self.__size)
