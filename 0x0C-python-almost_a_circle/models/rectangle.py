00e19cb3b772d6981e8b39b807b598fbb5175317
shrouk_ouda <shroukouda2@gmail.com>
2024-02-12 16:31:48 -0500

    commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,24 +75,23 @@ class Rectangle(Base):
             format(self.__class__.__name__, self.id,
                    self.x, self.y, self.width, self.height)
 
+    def __update(self, id=None, width=None, height=None, x=None, y=None):
+        if id is not None:
+            self.id = id
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
     def update(self, *args, **kwargs):
         if args:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.width = args[1]
-            if len(args) >= 3:
-                self.height = args[2]
-            if len(args) >= 4:
-                self.x = args[3]
-            if len(args) >= 5:
-                self.y = args[4]
+            self.__update(*args)
         elif kwargs:
-            self.id = kwargs.get("id", self.id)
-            self.width = kwargs.get("width", self.width)
-            self.height = kwargs.get("height", self.height)
-            self.x = kwargs.get("x", self.x)
-            self.y = kwargs.get("y", self.y)
+            self.__update(**kwargs)
 
     def to_dictionary(self):
         return {
