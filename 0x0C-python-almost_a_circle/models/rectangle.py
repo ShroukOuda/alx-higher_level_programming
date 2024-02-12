5f32d9555b8fd05eacc13ce1d3e75d0650ff073f
shrouk_ouda <shroukouda2@gmail.com>
2024-02-12 16:38:02 -0500

    commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-'''----'''
+'''---------'''
 from models.base import Base
 
 
 class Rectangle(Base):
-    '''---'''
+    '''------'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        '''------'''
         super().__init__(id)
         self.width = width
         self.height = height
@@ -15,67 +16,71 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        '''------'''
         return self.__width
 
     @width.setter
     def width(self, value):
-        self.validate_attributes("width", value, False)
+        self.validate_integer("width", value, False)
         self.__width = value
 
     @property
     def height(self):
+        '''--------------'''
         return self.__height
 
     @height.setter
     def height(self, value):
-        self.validate_attributes("height", value, False)
+        self.validate_integer("height", value, False)
         self.__height = value
 
     @property
     def x(self):
+        '''--------'''
         return self.__x
 
     @x.setter
     def x(self, value):
-        self.validate_attributes("x", value)
+        self.validate_integer("x", value)
         self.__x = value
 
     @property
     def y(self):
+        '''------------'''
         return self.__y
 
     @y.setter
     def y(self, value):
-        self.validate_attributes("y", value)
+        self.validate_integer("y", value)
         self.__y = value
 
-    def validate_attributes(self, name, value, eq=True):
+    def validate_integer(self, name, value, eq=True):
+        '''----------'''
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
-        if value < 0 and eq:
+        if eq and value < 0:
             raise ValueError("{} must be >= 0".format(name))
-        if value <= 0 and not eq:
+        elif not eq and value <= 0:
             raise ValueError("{} must be > 0".format(name))
 
     def area(self):
-        return self.__width * self.__height
+        '''---------------'''
+        return self.width * self.height
 
     def display(self):
-        for k in range(self.__y):
-            print()
-        for i in range(self.__height):
-            for m in range(self.__x):
-                print(' ', end='')
-            for j in range(self.__width):
-                print("#", end='')
-            print()
+        '''-------------'''
+        s = '\n' * self.y + \
+            (' ' * self.x + '#' * self.width + '\n') * self.height
+        print(s, end='')
 
     def __str__(self):
+        '''---------'''
         return '[{}] ({}) {}/{} - {}/{}'.\
-            format(self.__class__.__name__, self.id,
-                   self.x, self.y, self.width, self.height)
+            format(type(self).__name__, self.id, self.x, self.y, self.width,
+                   self.height)
 
     def __update(self, id=None, width=None, height=None, x=None, y=None):
+        '''---------------'''
         if id is not None:
             self.id = id
         if width is not None:
@@ -88,16 +93,13 @@ class Rectangle(Base):
             self.y = y
 
     def update(self, *args, **kwargs):
+        '''----------------------------'''
         if args:
             self.__update(*args)
         elif kwargs:
             self.__update(**kwargs)
 
     def to_dictionary(self):
-        return {
-                "id": self.id,
-                "width": self.__width,
-                "height": self.__height,
-                "x": self.__x,
-                "y": self.__y
-                }
+        '''---------------------'''
+        return {"id": self.id, "width": self.__width, "height": self.__height,
+                "x": self.__x, "y": self.__y}
