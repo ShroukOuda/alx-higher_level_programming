bbe3dec119dbc5fde8e9f2a380b3aabe5c6a3a6c
ShroukOuda <s36568961@gmail.com>
2024-05-22 14:42:28 -0700

    commit

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- list score, name order by desc
-SELECT score, name FROM second_table ORDER BY score, name DESC;
+SELECT score, name FROM second_table WHERE name != '' OR name IS NOT NULL ORDER BY score DESC;
 
