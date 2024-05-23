82fb4b88079beb127421d2c2c93a702a247ca0a7
ShroukOuda <s36568961@gmail.com>
2024-05-23 13:40:51 -0700

    commit

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- createdatabase and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT AUTO_INCREMENT PRIMARY KEY, 
+	constraint state_id INT FOREIGHN KEY REFERENCES state(id), 
+	name VARCHAR(256) NOT NULL);
+
