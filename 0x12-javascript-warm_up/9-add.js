6996b922764b4292a5c4f75ff649b1dcf8fa3df0
ShroukOuda <s36568961@gmail.com>
2024-06-21 14:26:37 +0300

    commit

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+function add (a, b) {
+  return (a + b);
+}
+const { argv } = require('node:process');
+const a = Number(argv[2]);
+const b = Number(argv[3]);
+console.log(add(a, b));
