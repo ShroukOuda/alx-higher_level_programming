ae9e9c4feed9043bdf4ea8ec446e3736c00f05eb
ShroukOuda <s36568961@gmail.com>
2024-06-21 10:23:31 +0300

    commit

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (argv[2] === undefined) { console.log('No argument'); }
-console.log(`${argv[2]}`);
+if (argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(`${argv[2]}`);
+}
