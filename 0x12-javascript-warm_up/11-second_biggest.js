3ce223edd36444067f0e02e56ec336f29e318a26
ShroukOuda <s36568961@gmail.com>
2024-06-22 10:34:17 +0300

    commit

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 const args = argv.slice(2);
-args.sort();
+args.sort((a, b) => a - b);
 if (args[args.length - 2] === undefined) { console.log(0); } else {
   console.log(args[args.length - 2]);
 }
